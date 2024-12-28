fdcd993f21124a083930610107fb3a7ed6a38f0e
lpiccinini25 <143562085+lpiccinini25@users.noreply.github.com>
2024-12-28 00:00:18 -0800

    Can now check tasks as done

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/Note.css"
+
 
 function Note({ note, onDelete }) {
     const formattedDate = new Date(note.created_at).toLocaleDateString("en-US")
 
+    const [completed, setCompleted] = useState(false);
+    const complete = (e) => {
+        console.log(e.target.checked);
+        setCompleted(e.target.checked);
+    };
     return (
         <div className="note-container">
-            <p className="note-title">{note.title}</p>
-            <p className="note-content">{note.content}</p>
+            <div class="title-checkbox-row">
+                <h1 class="title">{note.title}</h1>
+                <input 
+                type="checkbox" 
+                class="checkbox" 
+                id="exampleCheckbox" 
+                checked={completed}
+                onChange={complete}
+                />
+            </div>
             <p className="note-date">{formattedDate}</p>
             <button className="delete-button" onClick={() => onDelete(note.id)}>
                 Delete
